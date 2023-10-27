cb3a463af6998961b31f3169dac8ade426e34640
DimuthuMadushan <madushanwmd@gmail.com>
2023-10-27 17:50:14 +0530

    Add cli tool package

--- copybook-cli/src/main/java/io/ballerina/tools/copybook/cmd/CopybookCmd.java
+++ copybook-cli/src/main/java/io/ballerina/tools/copybook/cmd/CopybookCmd.java
@@ -6,25 +6,18 @@ import io.ballerina.tools.copybook.exception.CodeGenerationException;
 import io.ballerina.tools.copybook.generator.CopybookTypeGenerator;
 import picocli.CommandLine;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-import static java.lang.System.out;
-
-/**
- * Main class to implement "copybook" command for Ballerina.
- * Commands for Type generation.
- */
-@CommandLine.Command(name = "copybook", description = "Generates Ballerina record types for copybooks definition")
-public class CopybookCmd  implements BLauncherCmd {
+@CommandLine.Command(
+        name = "copybook",
+        description = "Generates Ballerina record types for copybooks definition"
+)
+public class CopybookCmd implements BLauncherCmd {
     private static final String CMD_NAME = "copybook";
     private final PrintStream outStream;
     private final boolean exitWhenFinish;
@@ -44,47 +37,24 @@ public class CopybookCmd  implements BLauncherCmd {
     @CommandLine.Parameters
     private List<String> argList;
 
-    /**
-     * Constructor that initialize with the default values.
-     */
     public CopybookCmd() {
         this(System.err, Paths.get(System.getProperty("user.dir")), true);
     }
 
-    /**
-     * Constructor override, which takes output stream and execution dir and exits when finish as inputs.
-     *
-     * @param outStream      output stream from ballerina
-     * @param executionDir   defines the directory location of  execution of ballerina command
-     * @param exitWhenFinish exit when finish the execution
-     */
     public CopybookCmd(PrintStream outStream, Path executionDir, boolean exitWhenFinish) {
         this.outStream = outStream;
         this.executionPath = executionDir;
         this.exitWhenFinish = exitWhenFinish;
     }
 
-    /**
-     * Exit with error code 1.
-     *
-     * @param exit Whether to exit or not.
-     */
     private static void exitError(boolean exit) {
         if (exit) {
             Runtime.getRuntime().exit(1);
         }
     }
-    @Override
-    public void printUsage(StringBuilder stringBuilder) {
-    }
-
-    @Override
-    public void setParentCmdParser(CommandLine parentCmdParser) {
-    }
 
     @Override
     public void execute() {
-        outStream.println("Generate ballerina types from a copybook definition file");
         try {
             if (helpFlag) {
                 String commandUsageInfo = BLauncherCmd.getCommandUsageInfo(getName());
@@ -99,17 +69,13 @@ public class CopybookCmd  implements BLauncherCmd {
         }
     }
 
-
     private void validateInputFlags() throws CmdException {
-        // Check if CLI input path flag argument is present
         if (inputPathFlag) {
-            // Check if Copybook definition file is provided
             if (argList == null) {
                 throw new CmdException("MESSAGE_FOR_MISSING_INPUT_ARGUMENT");
             }
         } else {
-            String commandUsageInfo = BLauncherCmd.getCommandUsageInfo(getName());
-            outStream.println(commandUsageInfo);
+            getCommandUsageInfo();
             exitError(this.exitWhenFinish);
         }
 
@@ -141,11 +107,6 @@ public class CopybookCmd  implements BLauncherCmd {
         CopybookTypeGenerator.generate(copybookFilePath, getTargetOutputPath(), outStream);
     }
 
-    /**
-     * Gets the target output path for the code generation.
-     *
-     * @return the target output path for the code generation
-     */
     private Path getTargetOutputPath() {
         Path targetOutputPath = executionPath;
         if (this.outputPath != null) {
@@ -162,6 +123,23 @@ public class CopybookCmd  implements BLauncherCmd {
         return filePath.endsWith("cpy");
     }
 
+    private void getCommandUsageInfo() {
+        String builder = "ballerina-copybook - Generate Ballerina types for given cobol copybook definitions\n\n" +
+                "bal copybook [-i | --input] <copybook-definition-file-path>\n" +
+                "                    [-o | --output] <output-location>\n\n";
+        outStream.println(builder);
+    }
+
+    @Override
+    public void printUsage(StringBuilder stringBuilder) {
+        stringBuilder.append("Ballerina Copybook tools - Code generation\n");
+        stringBuilder.append(
+                "Ballerina code generation for Copybook schema: bal copybook -i <copybook definition file path>\n");
+    }
+
+    @Override
+    public void setParentCmdParser(CommandLine parentCmdParser) {}
+
     @Override
     public String getName() {
         return CMD_NAME;
@@ -169,15 +147,5 @@ public class CopybookCmd  implements BLauncherCmd {
 
     @Override
     public void printLongDesc(StringBuilder stringBuilder) {
-        InputStream inputStream = ClassLoader.getSystemResourceAsStream("ballerina-copybook.help");
-        try (InputStreamReader inputStreamREader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-             BufferedReader br = new BufferedReader(inputStreamREader)) {
-            String content = br.readLine();
-            out.append(content);
-            while ((content = br.readLine()) != null) {
-                out.append('\n').append(content);
-            }
-        } catch (IOException ignore) {
-        }
     }
 }
